56762793945294a96c0213b8d405edaaa8a16311
Lipivalente <lipivalente1@gmail.com>
2026-06-19 00:01:21 -0300

    HomeFaculdade

--- src/App2.vue
+++ src/App2.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
     import Login from './Telas/Login.vue'
-    import TelaInicial from './Telas/components/TelaInicial.vue'
+    import HomeFaculdade from './Telas/HomeFaculdade.vue'
 </script>
 
 <template>
-        <TelaInicial/>
+        <HomeFaculdade title1="PLANO PEDAGÓGICO DE CURSO - PPC" title2="Faculdade de Engenharia da Computação"/>
 </template>
---
+++ src/Telas/HomeFaculdade.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+    const props = defineProps({
+        title1 : String,
+        title2 : String
+    })
+</script>
+
+<template>
+    <div class="div-container">
+        <h1>{{title1}}</h1>
+        <h3>{{ title2 }}</h3>
+
+        <button>Criar novo PPC</button>
+        <div class="div-container-list">
+
+        </div>
+    </div>
+</template>
+
+
+<style>
+    .div-container{
+        min-height: 100vh;
+        background-color: aqua;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+    .div-container-list{
+        background-color: bisque;
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+
+    h1{
+        margin-top: 15px;
+        font-size: 30px;
+    }
+
+    h3{
+        font-size: 20px;
+        margin-top: 15px;
+    }
+
+    button{
+        width: 200px;
+        height: 40px;
+        font-size: 16px;
+        margin-top: 30px;
+    }
+</style>
